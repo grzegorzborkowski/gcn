150294a5d9f4265d7a8ac0315cd3018468aa0891
Grzegorz Borkowski <gb.grzegorzborkowski@gmail.com>
2018-12-04 21:57:56 +0100

    Add negative links to dataset. Add epochs

--- Graphs.py
+++ Graphs.py
@@ -5,14 +5,16 @@ import abc
 import os
 import numpy as np
 import sklearn.model_selection
+import random
 
 class Graphs:
-
-    node_representation_size = 3
+    
+    node_representation_size = 64
     external_graph = None
     internal_graphs = {} # id_of_node_in_external_graph -> internal_graph
     train_to_valid_ratio = 0.7
     unique_internal_nodes = 2268
+    negative_to_positive_samples_ratio = 2.0
     
     @staticmethod
     def initialize():
@@ -72,10 +74,21 @@ class Graphs:
     @staticmethod
     def get_train_valid_examples():
         samples_X, samples_Y = [], []
+        number_of_positive_samples = 0
+        negative_samples_X = []
         for key, value in Graphs.external_graph.nodes.items():
             for neighbour in value.neighbours:
                 samples_X.append([key, neighbour.id])
                 samples_Y.append([0,1]) # like one-hot encoding
+                number_of_positive_samples+=1
+        for key, value1 in Graphs.external_graph.nodes.items():
+            for key_2, _ in Graphs.external_graph.nodes.items():
+                if not value1.contains_neighbour(key_2):
+                    negative_samples_X.append([key, key_2])
+        random.shuffle(negative_samples_X)
+        for i in range (0, (int)(len(samples_X)*Graphs.negative_to_positive_samples_ratio)):
+            samples_X.append([negative_samples_X[i][0], negative_samples_X[i][1]])
+            samples_Y.append([1, 0])
         samples_X, smaples_Y = np.array(samples_X), np.array(samples_Y)
         X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(samples_X, samples_Y)
         print (len(X_train))
@@ -138,6 +151,12 @@ class Node():
                 break
         if not node_exists_in_neighbour:
             node.neighbours.append(self)
+    
+    def contains_neighbour(self, id):
+        for neighbour in self.neighbours:
+            if neighbour.id == id:
+                return True
+        return False
 
     def __str__(self):
         return "Node: " + str(self.id) + " " +  str(len(self.neighbours)) #str(self.representation) + " : " + str(len(self.neighbours))
--- train2.py
+++ train2.py
@@ -12,52 +12,54 @@ Graphs.initialize()
 # Graphs.get_internal_graph(0).print_graph()
 train_X, test_X, train_y, test_y = Graphs.get_train_valid_examples()
 train_datasets = torch.utils.data.TensorDataset(train_X, train_y)
-train_loader = torch.utils.data.DataLoader(train_datasets, batch_size=1)
+train_loader = torch.utils.data.DataLoader(train_datasets, batch_size=8)
 
 model = DCNNv2()
 #loss_fn = F.binary_cross_entropy(size_average=False) # zmienic
 learning_rate = 0.001
+epochs = 20
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+for epoch_id in range(epochs):
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        # print (inputs)
+        # print (type(inputs))
+        y_pred = model(inputs)
 
-for i, data in enumerate(train_loader, 0):
-    inputs, labels = data
-    # print (inputs)
-    # print (type(inputs))
-    y_pred = model(inputs)
-
-  
-    loss = F.binary_cross_entropy(y_pred, labels)
-    # print (y_pred)
-    # print (labels)
-    print ("loss", i, loss.item())
-    # print (labels)
     
-    optimizer.zero_grad()
-    # print (optimizer)
-    loss.backward()#retain_graph=True)
-   
-    optimizer.step()
+        loss = F.binary_cross_entropy(y_pred, labels)
+        # print (y_pred)
+        # print (labels)
+        print ("epoch ", epoch_id, " loss", i, loss.item())
+        # print (labels)
+        
+        optimizer.zero_grad()
+        # print (optimizer)
+        loss.backward()#retain_graph=True)
     
-   
-    # print ("predicted_out", predict_out)
-   
-    #print ("predicted_maxed", predict_y)
-
-    # print ("predicted")
-    # print (predict_y)
-    # print (predict_y.shape)
-    # print ("test_y", test_y)
-    # print (test_y.shape)
-
-    for name, param in model.named_parameters():
-        pass
-        # if param.requires_grad:
-            # print (name, "\n", param.data, "\n", "grad", param.grad)
+        optimizer.step()
+        
     
-    #accuracy = accuracy_score(test_y, predict_y)
-    if i % 25 == 0:
-        predict_out = model(test_X)
-        predict_y = torch.round(predict_out)
-        correct = (predict_y == test_y).float().sum() 
-        print ("accuracy", correct.item()/(len(test_y)*2))
-    #print("accuracy", str(accuracy))
+        # print ("predicted_out", predict_out)
+    
+        #print ("predicted_maxed", predict_y)
+
+        # print ("predicted")
+        # print (predict_y)
+        # print (predict_y.shape)
+        # print ("test_y", test_y)
+        # print (test_y.shape)
+
+        for name, param in model.named_parameters():
+            pass
+            # if param.requires_grad:
+                # print (name, "\n", param.data, "\n", "grad", param.grad)
+        
+        #accuracy = accuracy_score(test_y, predict_y)
+        if i % 25 == 0:
+            pass
+            #predict_out = model(test_X)
+            #predict_y = torch.round(predict_out)
+            #correct = (predict_y == test_y).float().sum() 
+            #print ("accuracy", correct.item()/(len(test_y)*2))
+        #print("accuracy", str(accuracy))
