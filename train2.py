Fix problem of accuracy calculation

train2.py
@@ -16,31 +16,31 @@ train_loader = torch.utils.data.DataLoader(train_datasets, batch_size=1)
 
 model = DCNNv2()
 #loss_fn = F.binary_cross_entropy(size_average=False) # zmienic
-learning_rate = 0.0001
+learning_rate = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 for i, data in enumerate(train_loader, 0):
     inputs, labels = data
-    print (inputs)
-    print (type(inputs))
+    # print (inputs)
+    # print (type(inputs))
     y_pred = model(inputs)
 
   
     loss = F.binary_cross_entropy(y_pred, labels)
-    print (y_pred)
-    print (labels)
+    # print (y_pred)
+    # print (labels)
     print ("loss", i, loss.item())
-    print (labels)
+    # print (labels)
     
     optimizer.zero_grad()
-    print (optimizer)
+    # print (optimizer)
     loss.backward()#retain_graph=True)
    
     optimizer.step()
     
-    predict_out = model(test_X)
+   
     # print ("predicted_out", predict_out)
-    predict_y = torch.round(predict_out)
+   
     #print ("predicted_maxed", predict_y)
 
     # print ("predicted")
@@ -50,11 +50,14 @@ for i, data in enumerate(train_loader, 0):
     # print (test_y.shape)
 
     for name, param in model.named_parameters():
-        if param.requires_grad:
-            print (name, "\n", param.data, "\n", "grad", param.grad)
+        pass
+        # if param.requires_grad:
+            # print (name, "\n", param.data, "\n", "grad", param.grad)
     
     #accuracy = accuracy_score(test_y, predict_y)
     if i % 25 == 0:
+        predict_out = model(test_X)
+        predict_y = torch.round(predict_out)
         correct = (predict_y == test_y).float().sum() 
         print ("accuracy", correct.item()/(len(test_y)*2))
     #print("accuracy", str(accuracy))